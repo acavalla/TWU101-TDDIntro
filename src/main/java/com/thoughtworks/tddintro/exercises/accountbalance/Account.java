7d0642293115269768e07ca6ddc8d5213f02db1c
acavalla <anna.cavalla@gmail.com>
2021-10-14 19:10:21 +0100

    Wrote test for deposit and getBalance methods, test passing

--- src/main/java/com/thoughtworks/tddintro/exercises/accountbalance/Account.java
+++ src/main/java/com/thoughtworks/tddintro/exercises/accountbalance/Account.java
@@ -1,4 +1,11 @@
 package com.thoughtworks.tddintro.exercises.accountbalance;
 
 public class Account {
+    public int balance = 0;
+    public Account() {
+        this.balance = balance;
+    }
+
+    public void deposit(int x) { this.balance += x; }
+    public int getBalance() { return this.balance;}
 }
--- src/test/java/com/thoughtworks/tddintro/exercises/accountbalance/AccountTests.java
+++ src/test/java/com/thoughtworks/tddintro/exercises/accountbalance/AccountTests.java
@@ -3,17 +3,21 @@ package com.thoughtworks.tddintro.exercises.accountbalance;
 import org.junit.Ignore;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class AccountTests {
     @Test
-    @Ignore  // Remove each @Ignore and implement test
     public void shouldIncreaseMyBalanceWhenIDepositMoney(){
         Account account = new Account();
+        account.deposit(40);
+        assertEquals(account.getBalance(), 40);
     }
 
-    @Test
-    @Ignore  // Remove each @Ignore and implement test
+    @Test  // Remove each @Ignore and implement test
     public void shouldDecreaseMyBalanceWhenIWithdrawMoney(){
-
+        Account account = new Account();
+        account.withdraw(40);
+        assertEquals(account.getBalance(), -40);
     }
 
     @Test
