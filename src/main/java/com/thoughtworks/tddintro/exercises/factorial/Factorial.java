a51b004a4297f9ccdf227dc14ea8fc02f2ed24b9
acavalla <anna.cavalla@gmail.com>
2021-10-15 15:19:29 +0100

    All tests passing

--- src/main/java/com/thoughtworks/tddintro/exercises/factorial/Factorial.java
+++ src/main/java/com/thoughtworks/tddintro/exercises/factorial/Factorial.java
@@ -2,10 +2,11 @@ package com.thoughtworks.tddintro.exercises.factorial;
 
 public class Factorial {
     public Integer compute(int i) {
+        if (i < 0) throw new IllegalArgumentException();
         if (i < 2) {
             return 1;
         } else {
-            return i;
+            return i * compute(i-1);
         }
     }
 }
